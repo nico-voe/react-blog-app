3350e8c27f84a08af6ac1c18d776b0b423c4fe1d
Nico <96868692+nico-voe@users.noreply.github.com>
2022-06-21 13:57:28 +0200

    implementing useState

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
 function Home() {
+  // let name = 'nico'
+
+  const [name, setName] = useState('nico')
+
+  const handleClick = () => {
+    setName('not nico')
+
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <p>{name}</p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   )
 }
