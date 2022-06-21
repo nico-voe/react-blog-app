8c1dfbf5a647e1ef8d1ed8821569f997236d3ee3
Nico <you@example.com>
2022-06-21 09:34:48 +0200

    feature added
    added Navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Navbar />
+      <header className="content">
+        <h1>APP COMPONENT</h1>
       </header>
     </div>
   );
